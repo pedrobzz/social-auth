fb25cc83893ee4fc52efc18e24f2c2f48376e977
pedro bzz <pedrobzz66@gmail.com>
2022-07-26 08:39:54 -0300

    feat(Mutation - registerUser): created the mutation

--- src/server/application/controller/user.controller.ts
+++ src/server/application/controller/user.controller.ts
@@ -1,11 +1,16 @@
 import * as trpc from "@trpc/server";
 import { z } from "zod";
+import { makeCredentialsManager } from "../factories/makeCredentialsManager";
 
-export const userRouter = trpc.router().query("hello", {
-  input: z.string(),
-  resolve({ input }) {
-    return {
-      greeting: `Hello, ${input}!`,
-    };
+export const userRouter = trpc.router().mutation("registerUser", {
+  input: z.object({
+    name: z.string().max(50).min(3),
+    email: z.string().email(),
+    password: z.string().min(8),
+  }),
+  async resolve({ input }) {
+    const { name, email, password } = input;
+    const credentialsManager = makeCredentialsManager();
+    return await credentialsManager.createUser({ name, email, password });
   },
 });
