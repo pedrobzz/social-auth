29600e902ceb8d4e72529093eb365dcc830e3663
pedro bzz <pedrobzz66@gmail.com>
2022-07-26 19:56:16 -0300

    domain(userRepository): Updated the returns to include the username and added a "updateUser" method

--- src/server/domain/repositories/userRepository.ts
+++ src/server/domain/repositories/userRepository.ts
@@ -1,23 +1,70 @@
 /* eslint-disable @typescript-eslint/no-namespace */
+
 import { BaseServerResponse } from "../baseResponse";
+import { RequireAtLeastOne } from "../utils";
 
 export interface UserRepositoryModel {
-  getUserById(
-    id: string,
-  ): Promise<BaseServerResponse<{ id: string; name: string; email: string }>>;
-  getUserByEmail(
-    email: string,
-  ): Promise<BaseServerResponse<{ id: string; name: string; email: string }>>;
-  getUserByUsername(
-    username: string,
-  ): Promise<BaseServerResponse<{ id: string; name: string; email: string }>>;
+  getUserById(id: string): Promise<
+    BaseServerResponse<{
+      id: string;
+      name: string;
+      email: string;
+      username: string | null;
+    }>
+  >;
+
+  getUserByEmail(email: string): Promise<
+    BaseServerResponse<{
+      id: string;
+      name: string;
+      email: string;
+      username: string | null;
+    }>
+  >;
+
+  getUserByUsername(username: string): Promise<
+    BaseServerResponse<{
+      id: string;
+      name: string;
+      email: string;
+      username: string | null;
+    }>
+  >;
+
   registerUser(user: {
     name: string;
     password: string;
     email: string;
-  }): Promise<BaseServerResponse<{ id: string; name: string; email: string }>>;
+  }): Promise<
+    BaseServerResponse<{
+      id: string;
+      name: string;
+      email: string;
+      username: string | null;
+    }>
+  >;
+
   checkUserPassword(user: {
     email: string;
     password: string;
   }): Promise<BaseServerResponse>;
+
+  updateUser(
+    userId: string,
+    update: RequireAtLeastOne<{
+      email: string;
+      emailVerified: Date;
+      name: string;
+      username: string | null;
+      image: string;
+      password: string;
+    }>,
+  ): Promise<
+    BaseServerResponse<{
+      id: string;
+      name: string;
+      email: string;
+      username: string | null;
+    }>
+  >;
 }
