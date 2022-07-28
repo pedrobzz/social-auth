cd3f017c61de69606410f883577a6017752167f1
pedro bzz <pedrobzz66@gmail.com>
2022-07-28 07:11:12 -0300

    feat(updateUsername): implemented the updateUsername onTRPC

--- src/server/application/controller/user.controller.ts
+++ src/server/application/controller/user.controller.ts
@@ -2,15 +2,28 @@ import * as trpc from "@trpc/server";
 import { z } from "zod";
 import { makeCredentialsManager } from "../factories/makeCredentialsManager";
 
-export const userRouter = trpc.router().mutation("registerUser", {
-  input: z.object({
-    name: z.string().max(50).min(3),
-    email: z.string().email(),
-    password: z.string().min(8),
-  }),
-  async resolve({ input }) {
-    const { name, email, password } = input;
-    const credentialsManager = makeCredentialsManager();
-    return await credentialsManager.createUser({ name, email, password });
-  },
-});
+export const userRouter = trpc
+  .router()
+  .mutation("registerUser", {
+    input: z.object({
+      name: z.string().max(50).min(3),
+      email: z.string().email(),
+      password: z.string().min(8),
+    }),
+    async resolve({ input }) {
+      const { name, email, password } = input;
+      const credentialsManager = makeCredentialsManager();
+      return await credentialsManager.createUser({ name, email, password });
+    },
+  })
+  .mutation("updateUsername", {
+    input: z.object({
+      id: z.string(),
+      username: z.string().max(50).min(3),
+    }),
+    resolve({ input }) {
+      const { id, username } = input;
+      const credentialsManager = makeCredentialsManager();
+      return credentialsManager.changeUsername({ id, newUsername: username });
+    },
+  });
--- src/server/application/use-cases/credentials-manager/credentialsManager.spec.ts
+++ src/server/application/use-cases/credentials-manager/credentialsManager.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { UserRepositoryModel } from "../../../domain/repositories/userRepository";
@@ -5,26 +6,28 @@ import { BaseServerResponse } from "../../../domain/baseResponse";
 import { User } from "@prisma/client";
 import { CredentialsManager } from "./credentialsManager";
 import { verify } from "argon2";
+import { RequireAtLeastOne } from "../../../domain/utils";
 
 class UserRepositoryMock implements UserRepositoryModel {
   users: User[] = [];
-  getUserById(
-    id: string,
-  ): Promise<BaseServerResponse<{ id: string; name: string; email: string }>> {
-    throw new Error("Method not implemented.");
-  }
-  getUserByEmail(
-    email: string,
-  ): Promise<BaseServerResponse<{ id: string; name: string; email: string }>> {
+  getUserById(id: string): Promise<
+    BaseServerResponse<{
+      id: string;
+      name: string;
+      email: string;
+      username: string | null;
+    }>
+  > {
     return new Promise(resolve => {
-      const user = this.users.find(u => u.email === email);
-      if (user) {
+      const userExists = this.users.find(u => u.id === id);
+      if (userExists) {
         resolve({
           status: 200,
           data: {
-            id: user.id,
-            name: user.name,
-            email: user.email,
+            id: userExists.id,
+            name: userExists.name,
+            email: userExists.email,
+            username: userExists.username,
           },
         });
       } else {
@@ -35,16 +38,75 @@ class UserRepositoryMock implements UserRepositoryModel {
       }
     });
   }
-  getUserByUsername(
-    username: string,
-  ): Promise<BaseServerResponse<{ id: string; name: string; email: string }>> {
-    throw new Error("Method not implemented.");
+
+  getUserByEmail(email: string): Promise<
+    BaseServerResponse<{
+      id: string;
+      name: string;
+      email: string;
+      username: string | null;
+    }>
+  > {
+    return new Promise(resolve => {
+      const user = this.users.find(u => u.email === email);
+      if (user) {
+        resolve({
+          status: 200,
+          data: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            username: user.username,
+          },
+        });
+      } else {
+        resolve({
+          status: 404,
+          message: "User with this Email not found",
+        });
+      }
+    });
+  }
+  getUserByUsername(username: string): Promise<
+    BaseServerResponse<{
+      id: string;
+      name: string;
+      email: string;
+      username: string | null;
+    }>
+  > {
+    return new Promise(resolve => {
+      const user = this.users.find(u => u.username === username);
+      if (user) {
+        resolve({
+          status: 200,
+          data: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            username: user.username,
+          },
+        });
+      } else {
+        resolve({
+          status: 404,
+          message: "User with this Email not found",
+        });
+      }
+    });
   }
   registerUser(user: {
     name: string;
     password: string;
     email: string;
-  }): Promise<BaseServerResponse<{ id: string; name: string; email: string }>> {
+  }): Promise<
+    BaseServerResponse<{
+      id: string;
+      name: string;
+      email: string;
+      username: string | null;
+    }>
+  > {
     return new Promise(resolve => {
       if (this.users.find(u => u.email === user.email)) {
         resolve({
@@ -68,6 +130,7 @@ class UserRepositoryMock implements UserRepositoryModel {
           id: newUser.id,
           name: newUser.name,
           email: newUser.email,
+          username: newUser.username,
         },
       });
     });
@@ -91,6 +154,56 @@ class UserRepositoryMock implements UserRepositoryModel {
           message: "Wrong password",
         };
       }
+    } else {
+      return {
+        status: 404,
+        message: "User with this Email not found",
+      };
+    }
+  }
+
+  async updateUser(
+    userId: string,
+    update: RequireAtLeastOne<
+      {
+        email: string;
+        emailVerified: Date;
+        name: string;
+        username: string;
+        image: string;
+        password: string;
+      },
+      "name" | "email" | "username" | "password" | "emailVerified" | "image"
+    >,
+  ): Promise<
+    BaseServerResponse<{
+      id: string;
+      name: string;
+      email: string;
+      username: string | null;
+    }>
+  > {
+    const userExists = this.users.find(u => u.id === userId);
+    if (userExists) {
+      this.users = this.users.map(u => {
+        if (u.id === userId) {
+          return {
+            ...u,
+            ...update,
+          };
+        }
+        return u;
+      });
+      const updatedUser = this.users.find(u => u.id === userId)!;
+      return {
+        status: 200,
+        data: {
+          id: updatedUser.id,
+          name: updatedUser.name,
+          email: updatedUser.email,
+          username: updatedUser.username,
+        },
+      };
     } else {
       return {
         status: 404,
@@ -122,6 +235,7 @@ describe("Credentials Manager", () => {
       id: "1",
       name: "John Doe",
       email: "john@doe.dev",
+      username: null,
     });
   });
 
@@ -182,5 +296,57 @@ describe("Credentials Manager", () => {
     expect(result.data?.email).toEqual(user.email);
     expect(result.data?.name).toEqual(user.name);
     expect(result.message).toEqual("User logged in successfully");
+  });
+
+  it("changeUsername - Should return 404 if informs an invalid ID", async () => {
+    const { sut } = makeSUT();
+    const result = await sut.changeUsername({
+      id: "1123123",
+      newUsername: "test",
+    });
+    expect(result.status).toBe(404);
+    expect(result.message).toBe("User with this ID not found");
+  });
+
+  it("changeUsername - Should return 200 if successfully changed the username", async () => {
+    const { sut, userRepository } = makeSUT();
+    const { data: user } = await sut.createUser({
+      name: "John Doe",
+      email: "john@doe.dev",
+      password: "12345",
+    });
+    const result = await sut.changeUsername({
+      id: user!.id,
+      newUsername: "test",
+    });
+    expect(result.status).toBe(200);
+    expect(result.data?.id).toEqual(user!.id);
+    expect(result.data?.name).toEqual(user!.name);
+    expect(result.data?.email).toEqual(user!.email);
+    expect(result.data?.username).toEqual("test");
+  });
+
+  it("changeUsername - Should return 400 if the username is already taken", async () => {
+    const { sut, userRepository } = makeSUT();
+    const { data: user } = await sut.createUser({
+      name: "John Doe",
+      email: "john@doe.dev",
+      password: "12345",
+    });
+    const { data: user2 } = await sut.createUser({
+      name: "John Doe",
+      email: "john2@doe.dev",
+      password: "12345",
+    });
+    await sut.changeUsername({
+      id: user!.id,
+      newUsername: "test",
+    });
+    const result = await sut.changeUsername({
+      id: user2!.id,
+      newUsername: "test",
+    });
+    expect(result.status).toBe(400);
+    expect(result.message).toBe("User with this Username already exists");
   });
 });
